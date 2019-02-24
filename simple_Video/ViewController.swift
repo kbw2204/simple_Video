fc868f221607ff65fc36365624173db20991726e
강병우 <kbw2204@naver.com>
2019-02-24 15:25:05 +0900

    Initial commit

---
+++ simple_Video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple_Video
+//
+//  Created by 강병우 on 24/02/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
